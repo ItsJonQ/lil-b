b6f3a4dd517e2eb466b72cfabbec0a4341b226bb
ItsJonQ <itsjonq@gmail.com>
2014-04-24 00:30:25 -0400

    Updated banner. Added grunt-contrib-concat to the project. Adjusted Grunt tasks to work with concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,31 +2,56 @@ module.exports = function(grunt) {
 
     // Configuration goes here
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        cssmin: {
-            add_banner: {
-                options: {
-                  banner: '/*!\n * Lil B - Bootstrap Reset\n * Jon Q. 2014.\n * https://github.com/ItsJonQ/bootstrap-lil-b\n */'
-              },
-              files: {
-                  'b.min.css': ['b.css']
-              }
-          }
+
+      pkg: grunt.file.readJSON('package.json'),
+
+      banner: '/*!\n' +
+      ' * Lil\' B - Bootstrap\n' +
+      ' * A Bootstrap for Bootstrap\n' +
+      ' * v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+      ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+      ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+      ' */\n\n',
+
+      concat: {
+        options: {
+          banner: '<%= banner %>',
+          separator: '\n\n',
         },
-        watch: {
-            scripts: {
-                files: ['b.css'],
-                tasks: ['cssmin']
-            }
+        dist: {
+          src: [
+            'src/css/b.typgraphy.css',
+            'src/css/b.optimize.css',
+            'src/css/b.control.css',
+            'src/css/b.helpers.css'],
+          dest: 'dist/css/b.css',
+        },
+      },
+
+      cssmin: {
+        minify: {
+          files: {
+            'dist/css/b.min.css': ['dist/css/b.css']
+          }
         }
+      },
+
+      watch: {
+        scripts: {
+          files: ['src/**/*.css'],
+          tasks: ['concat', 'cssmin']
+        }
+      }
+
     });
 
     // Load plugins here
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
 
     // Define your tasks here
-    grunt.registerTask('default', ['cssmin', 'watch']);
+    grunt.registerTask('default', ['concat', 'cssmin']);
 
-};
+  };
